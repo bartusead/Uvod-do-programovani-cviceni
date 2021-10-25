7d9635cefbcb1477b38741cd9ccd7eb58ecb4fa6
Adam Bartůšek <bartusek.a@seznam.cz>
2021-10-25 20:07:43 +0200

    kolečko+souřadnice

--- 1.DU/DU1.py
+++ 1.DU/DU1.py
@@ -1,4 +1,4 @@
-from turtle import forward, left, right, penup, pendown, setpos, speed
+from turtle import forward, left, right, penup, pendown, setpos, speed, exitonclick, circle
 a = 100
 speed(0)
 for _ in range(2):
@@ -20,3 +20,20 @@ for _ in range(3):
             forward(a)
             left(90)
         forward(a)
+penup()
+
+x = int(input("Zadej souřadnici x:"))
+y = int(input("Zadej souřadnici y:"))
+
+setpos(x,y)
+pendown()
+circle(50)
+
+
+
+
+
+exitonclick()
+
+
+
